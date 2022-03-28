f8d5fe9b711f8db30b73f563184b710e7cf5e1de
amarahanieka <amarahanieka30@gmail.com>
2022-03-28 12:23:22 +0700

    feat: add book + BE integration

---
+++ ENSIKLO/ENSIKLO/ENSIKLO/Renderers/RoundedEditor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ENSIKLO.Renderers
+{
+    public class RoundedEditor : Editor
+    {
+        //empty or define your custom fields
+    }
+}
