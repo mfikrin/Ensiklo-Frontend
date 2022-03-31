6562215416eaf05086137242b7ea4dac5de09a8f
mfikrin <muhammad.fikrin1729@gmail.com>
2022-03-31 11:30:36 +0700

    feat : separate view for user and admin

--- ENSIKLO/ENSIKLO/ENSIKLO/ViewModels/LoginViewModel.cs
+++ ENSIKLO/ENSIKLO/ENSIKLO/ViewModels/LoginViewModel.cs
@@ -17,15 +17,31 @@ namespace ENSIKLO.ViewModels
         public LoginViewModel(IBookService bookService)
         {
             _bookService = bookService;
-            LoginCommand = new Command(OnLoginClicked);
+            LoginCommand = new Command<string>(OnLoginClicked);
 
             TappedCommand = new Command(onTapped);
         }
 
-        private async void OnLoginClicked(object obj)
+        private async void OnLoginClicked(string role)
         {
             // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//main/home");
+
+            // NANTI UNCOMMENT INI
+
+            //if (role.Equals("Admin"){
+            //    await Shell.Current.GoToAsync($"//admin/homeAdmin");
+            //}
+            //else if (role.Equals("User"))
+            //{
+            //    await Shell.Current.GoToAsync($"//main/home");
+            //}
+
+
+
+            await Shell.Current.GoToAsync($"//admin/homeAdmin");
+            //await Shell.Current.GoToAsync($"//main/home");
+            //await Shell.Current.GoToAsync($"//admin/homeAdmin");
+
         }
 
         private async void onTapped(object obj)
