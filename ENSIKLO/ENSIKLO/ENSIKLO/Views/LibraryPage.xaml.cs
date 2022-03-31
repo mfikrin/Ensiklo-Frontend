cf3d2e74dbeb58a68241ef02bdd43c99c0ccb5ca
nabelanita <13519104@mahasiswa.itb.ac.id>
2022-04-01 05:43:50 +0700

    library base code

---
+++ ENSIKLO/ENSIKLO/ENSIKLO/Views/LibraryPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ENSIKLO.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ENSIKLO.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LibraryPage : ContentPage
+    {
+        private readonly BookViewModel _booksViewModel;
+        public LibraryPage()
+        {
+            InitializeComponent();
+            _booksViewModel = Startup.Resolve<BookViewModel>();
+            BindingContext = _booksViewModel;
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _booksViewModel.OnAppearing();
+            _booksViewModel?.PopulateBooks();
+        }
+    }
+}
+
