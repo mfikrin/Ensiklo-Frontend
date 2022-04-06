b0b622fb7967347a256558f726f8e9642a8e24e7
mfikrin <muhammad.fikrin1729@gmail.com>
2022-04-06 18:39:05 +0700

    refactor : remove routing for admin

--- ENSIKLO/ENSIKLO/ENSIKLO/ViewModels/LoginViewModel.cs
+++ ENSIKLO/ENSIKLO/ENSIKLO/ViewModels/LoginViewModel.cs
@@ -19,30 +19,16 @@ namespace ENSIKLO.ViewModels
         public LoginViewModel(IUserService userService)
         {
             UserServiceLogin = userService;
-            LoginCommand = new Command<string>(OnLoginClicked);
+            LoginCommand = new Command(OnLoginClicked);
 
             TappedCommand = new Command(onTapped);
         }
 
-        private async void OnLoginClicked(string role)
+        private async void OnLoginClicked()
         {
-            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-
-            // NANTI UNCOMMENT INI
-
-            //if (role.Equals("Admin"){
-            //    await Shell.Current.GoToAsync($"//admin/homeAdmin");
-            //}
-            //else if (role.Equals("User"))
-            //{
-            //    await Shell.Current.GoToAsync($"//main/home");
-            //}
-
-
-
-            await Shell.Current.GoToAsync("//admin/homeAdmin");
-            //await Shell.Current.GoToAsync($"//main/home");
-            //await Shell.Current.GoToAsync($"//admin/homeAdmin");
+ 
+            await Shell.Current.GoToAsync("//main/home");
+            
 
         }
 
